03300d0f0a7c56c119fccc930356e23865d56c0e
L-Anjo <a23528@alunos.ipca.pt>
2023-12-05 22:20:18 +0000

    Alterações Design Detail da House

---
+++ app/src/main/java/ipca/utility/bookinghousesapp/HouseDetailActivity.kt
@@ -0,0 +1,11 @@
+package ipca.utility.bookinghousesapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HouseDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_housedetail)
+    }
+}
