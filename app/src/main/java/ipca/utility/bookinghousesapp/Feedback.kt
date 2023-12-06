778c5186251e05c5ac0b1d5a3be81d7fdf8092c6
Anjo <luisanjo17@gmail.com>
2023-12-06 22:59:25 +0000

    Melhoria

---
+++ app/src/main/java/ipca/utility/bookinghousesapp/Feedback.kt
@@ -0,0 +1,7 @@
+package ipca.utility.bookinghousesapp
+
+data class Feedback (
+    var id_feedback : Int,
+    var classification : Int,
+    var comment : String
+)
--- app/src/main/java/ipca/utility/bookinghousesapp/HouseDetailActivity.kt
+++ app/src/main/java/ipca/utility/bookinghousesapp/HouseDetailActivity.kt
@@ -2,10 +2,48 @@ package ipca.utility.bookinghousesapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.TextView
+import ipca.utility.bookinghousesapp.databinding.ActivityHousedetailBinding
 
 class HouseDetailActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityHousedetailBinding
+    var feedback = arrayListOf<Feedback>()
+    val feedbackAdapter = FeedbackAdapter()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_housedetail)
+        binding = ActivityHousedetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        feedback.add(Feedback(1,4,"Muito Bom"))
+        feedback.add(Feedback(2,4,"Muito Bom"))
+        binding.listViewFeebackDetails.adapter = feedbackAdapter
+    }
+
+    inner class FeedbackAdapter : BaseAdapter(){
+        override fun getCount(): Int {
+            return feedback.size
+        }
+
+        override fun getItem(position: Int): Any {
+            return feedback[position]
+        }
+
+        override fun getItemId(position: Int): Long {
+            return 0
+        }
+
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+            val rootView = layoutInflater.inflate(R.layout.row_feeback_housedetail,parent, false)
+            rootView.findViewById<TextView>(R.id.textViewNomeFeedback).text = feedback[position].id_feedback.toString()
+            rootView.findViewById<TextView>(R.id.textViewClassiFeedback).text = feedback[position].classification.toString()
+            rootView.findViewById<TextView>(R.id.textViewComentFeedback).text = feedback[position].comment
+
+            return rootView
+        }
+
     }
 }
