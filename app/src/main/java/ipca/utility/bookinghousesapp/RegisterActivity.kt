a0963ac3acf07242ee8f63759467f0c9f578e415
Pedro Martins <a23527@alunos.ipca.pt>
2023-12-16 20:33:30 +0000

    Login and Register Design

--- app/src/main/java/ipca/utility/bookinghousesapp/RegisterActivity.kt
+++ app/src/main/java/ipca/utility/bookinghousesapp/RegisterActivity.kt
@@ -1,36 +1,35 @@
 package ipca.utility.bookinghousesapp
 
-import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.EditText
-import ipca.utility.bookinghousesapp.databinding.ActivityHousedetailBinding
 import ipca.utility.bookinghousesapp.databinding.ActivityLoginBinding
+import ipca.utility.bookinghousesapp.databinding.ActivityRegisterBinding
 
-class LoginActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityLoginBinding
+    private lateinit var binding : ActivityRegisterBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityLoginBinding.inflate(layoutInflater)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
-
-
-        binding.buttonLogin.setOnClickListener{
+        binding.buttonRegister.setOnClickListener{
+            val editTextName = binding.editTextName.text.toString()
             val editTextEmail = binding.editTextEmail.text.toString()
             val editTextPassword = binding.editTextPassword.text.toString()
+            val editTextPhone = binding.editTextPhone.text.toString()
 
+            Log.d("name", editTextName)
             Log.d("email", editTextEmail)
             Log.d("password", editTextPassword)
+            Log.d("phone", editTextPhone)
         }
 
-        binding.textViewRegister.setOnClickListener{
-            val intent = Intent(this,RegisterActivity::class.java )
+        binding.textViewLogin.setOnClickListener{
+            val intent = Intent(this,LoginActivity::class.java )
             startActivity(intent)
         }
     }
