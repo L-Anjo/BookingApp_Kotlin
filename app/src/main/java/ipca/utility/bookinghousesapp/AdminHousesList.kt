6b7468f7b96de33e8fd9c2ab5f2e4d36fb7e2910
Pedro Martins <a23527@alunos.ipca.pt>
2023-12-18 18:50:21 +0000

    Admin houses list create

---
+++ app/src/main/java/ipca/utility/bookinghousesapp/AdminHousesList.kt
@@ -0,0 +1,11 @@
+package ipca.utility.bookinghousesapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AdminHousesList : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_admin_houses_list)
+    }
+}
